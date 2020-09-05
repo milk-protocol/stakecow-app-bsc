7117df69ee87a9db7fcd0cc1be1d4129aedb0511
stakecow <stakecow@protonmail.com>
2020-09-05 14:55:15 +0800

    Add contract addresses

--- pages/about.vue
+++ pages/about.vue
@@ -17,6 +17,12 @@
         <div>Genxi: <b>2000</b> MILK supplied, halve every week, first week supply is 1000 MILK.</div>
         <div>Holstein: <b>9282</b> MILK supplied, increase 10% everyweek, first week supply is 2000 MILK.</div>
 
+        <br>
+        <br>
+        <h4>Contracts</h4>
+        <div>MILK: <a href="https://bscscan.com/address/0x8e9f5173e16ff93f81579d73a7f9723324d6b6af" target="_blank">0x8e9f5173e16ff93f81579d73a7f9723324d6b6af</a></div>
+        <div>CowFactory: <a href="https://bscscan.com/address/0x4BF38c704675a148E4ECb8142B8e505591092E97" target="_blank">0x4BF38c704675a148E4ECb8142B8e505591092E97</a></div>
+        <div>Angier Cow: <a href="https://bscscan.com/address/0xA789383b848BcD7c60975094AE3ba5444ED70103" target="_blank">0xA789383b848BcD7c60975094AE3ba5444ED70103</a></div>
 
       </div>
     </div>
