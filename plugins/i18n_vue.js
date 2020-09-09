f46cc3e15f94ac505ef952b91e68700471efffb8
stakecow <stakecow@protonmail.com>
2020-09-09 15:05:03 +0800

    auto-language

--- plugins/i18n_vue.js
+++ plugins/i18n_vue.js
@@ -4,7 +4,14 @@ import VueI18n from 'vue-i18n'
 Vue.use(VueI18n)
 
 export default ({ app, store }) => {
-  let language = window.localStorage.language ? JSON.parse(window.localStorage.language) : 'en';
+  let language = window.localStorage.language ? JSON.parse(window.localStorage.language) : (navigator.browserLanguage ? navigator.browserLanguage : navigator.language);
+  if (language === 'en-US') {
+    language = 'en'
+  } else if (language === 'zh-CN' || language === 'zh-cn') {
+    language = 'zh_CN'
+  } else if (language !== 'en' && language !== 'zh') {
+    language = 'en'
+  }
   store.commit("updateLang", language)
   app.i18n = new VueI18n({
     locale: language,
